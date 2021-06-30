06bed3edf3aa4252aa11f046cec42fff0d2fe001
santosh <santosh.ece06@gmail.com>
2021-06-30 10:44:09 +0530

    useState.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  // let tempCounter = 100;
+  // 1. This is a method
+  // 2. This method is special method; since it starts with USE PREFIX
+  // 3. this special method is also known as HOOKS in react.
+  let output = useState(100);
+  console.log(output);
+
+  // DESTRUCTING IS HAPPENING.
+  let [counter, setCounter] = useState(200);
+  let increment = () => {
+    setCounter(counter + 100);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World</h1>
+      <h1>{counter}</h1>
+      <h1>
+        <button onClick={increment}>INCRMENT</button>
+      </h1>
     </div>
   );
 }
